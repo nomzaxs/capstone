93ce7fbd51beb6810e39692410bbe2f49ccbe73a
nomzaxs <nomzaxs@gmail.com>
2021-05-26 18:06:01 +0900

    25작업

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.capstone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this, R.id.main_fragment);
+
+        if (getSupportActionBar() != null)getSupportActionBar().hide()
     }
 }
