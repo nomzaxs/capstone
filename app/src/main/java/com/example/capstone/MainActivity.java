2087213ecef6a3b28fe0d578ef37a0af90e4669b
nomzaxs <nomzaxs@gmail.com>
2021-05-27 16:53:05 +0900

    27일 수정

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -5,6 +5,11 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +17,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         navController = Navigation.findNavController(this, R.id.main_fragment);
 
-        if (getSupportActionBar() != null)getSupportActionBar().hide()
+        if (getSupportActionBar() != null)getSupportActionBar().hide();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        navController.navigateUp();
+        return true;
     }
 }
